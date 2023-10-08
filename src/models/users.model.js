3c6fdfadccf2095f70a144cbaec22d4f1c2d91f3
kshlove735 <kshlove735@gmail.com>
2023-10-08 16:42:36 +0900

    비밀번호 암호화

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -20,18 +20,33 @@ const userSchema = mongoose.Schema({
     }
 })
 
+userSchema.pre('save', function (next) {
+    let user = this;
+    // 비밀번호 변경될 때만
+    if (user.isModified('password')) {
+        // salt 생성
+        bcrypt.genSalt(Number(process.env.SALTROUNDS), function (err, salt) {
+            if (err) return next(err);
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+                user.password = hash;
+                next();
+            })
+        })
+    }
+})
+
 
 // ! 4
 userSchema.methods.comparePassword = function (plainPassword, cb) {
-    // TODO bcrypt compare 비교
+    // bcrypt compare 비교
     // plain password  => client, this.password => DB에 있는 비밀번호
-    if (plainPassword === this.password) {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
-
-    return cb({ error: 'error' });
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        console.log(isMatch);
+        cb(null, isMatch);
+    })
 }
 
 const User = mongoose.model('User', userSchema);
